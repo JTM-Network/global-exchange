333271a9155ecfbf1325b04ae1b76aa2c78cd69b
Joe Matthews <joetymatthews@gmail.com>
2023-03-01 01:42:32 +0000

    new: main & test suite classes.
    - register plugin yml & messages.

---
+++ src/main/java/com/jtmnetwork/exchange/GlobalExchange.java
@@ -0,0 +1,30 @@
+package com.jtmnetwork.exchange;
+
+import com.jtm.framework.Framework;
+
+public class GlobalExchange extends Framework {
+
+    public GlobalExchange() {
+        super(false, false, false);
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+}
---
+++ src/test/java/com/jtmnetwork/exchange/GlobalExchangeTestSuite.java
@@ -0,0 +1,9 @@
+package com.jtmnetwork.exchange;
+
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(value = {})
+public class GlobalExchangeTestSuite {}
